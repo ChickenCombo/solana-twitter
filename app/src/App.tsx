1f0ede854a4cb0c2d2e52dae387e5d556e416e26
Lester Cuadra <johnlestercuadra.dev@gmail.com>
2024-03-03 12:39:32 +0800

    chore: initial client

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen w-screen ">
+        <div className="flex h-full w-full items-center justify-center">
+          Hello, world!
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
